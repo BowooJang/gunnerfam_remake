86f3525559493866271eca5bd726c22a5deba238
SEo102 <43981163+BowooJang@users.noreply.github.com>
2022-02-23 13:52:14 +0900

    메뉴만 떠야하는데 컨텐츠까지 표시되는 현상 수정

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const button = document.querySelector('.menu_button');
 const menu = document.querySelector('.menu');
+const content = document.querySelector('.content');
+
 
 button.addEventListener('click', () => {
     menu.classList.toggle('active');
+    content.classList.toggle('active');
+
 });
